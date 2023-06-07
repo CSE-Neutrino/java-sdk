37a59982a5ffdf5f6af3c477614333616351aed1
Mahmut Canga <cangamahmut@gmail.com>
2023-06-07 16:08:10 +0100

    added workflow activity implementation

---
+++ examples/src/main/java/io/dapr/examples/workflows/DemoWorkflowActivity.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2023 The Dapr Authors
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package io.dapr.examples.workflows;
+
+import io.dapr.workflows.runtime.WorkflowActivity;
+import io.dapr.workflows.runtime.WorkflowActivityContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public class DemoWorkflowActivity implements WorkflowActivity {
+
+  Logger logger = LoggerFactory.getLogger(DemoWorkflowActivity.class);
+
+  @Override
+  public DemoActivityOutput run(WorkflowActivityContext ctx) {
+    logger.info("Starting Activity: " + ctx.getName());
+
+    var message = ctx.getInput(DemoActivityInput.class).getMessage();
+    var newMessage = message + " - Activity finished";
+    logger.info("Message Received from input: " + message);
+
+    try {
+      TimeUnit.SECONDS.sleep(5);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+
+    var output = new DemoActivityOutput(message, newMessage);
+    logger.info("Message sent to output: " + newMessage);
+
+    logger.info("Activity finished");
+
+    return output;
+  }
+}
